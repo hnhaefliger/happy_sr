bd6c1c81734ca2d60fdb7ffe30ca84b903822459
hnhaefliger <henry_haefliger@protonmail.com>
2021-10-04 17:35:56 -0400

    change layer dimensions

--- happy_sr/model/model.py
+++ happy_sr/model/model.py
@@ -5,32 +5,30 @@ class Model(torch.nn.Module):
     def __init__(self, n_cnn_layers, n_rnn_layers, rnn_dim, n_class, n_feats, stride=2, dropout=0.1):
         super(Model, self).__init__()
         self.hidden1 = torch.nn.Sequential(
-            torch.nn.Linear(1024, 1024),
+            torch.nn.Linear(64, 128),
             torch.nn.ReLU(),
             torch.nn.Dropout(0.05),
-            torch.nn.Linear(1024, 1024),
+            torch.nn.Linear(128, 128),
             torch.nn.ReLU(),
             torch.nn.Dropout(0.05),
-            torch.nn.Linear(1024, 1024),
+            torch.nn.Linear(128, 128),
             torch.nn.ReLU(),
             torch.nn.Dropout(0.05),
         )
         self.rnn = torch.nn.Sequential(
-            torch.nn.RNN(512, 512, bidirectional=True),
+            torch.nn.RNN(128, 128, bidirectional=True),
             torch.nn.Dropout(0.50),
         )
         self.hidden2 = torch.nn.Sequential(
-            torch.nn.Linear(512, 29)
+            torch.nn.Linear(128, 29)
         )
 
     def forward(self, x):
-        print(x.size())
-        x = torch.transpose(x, 1, 2)
-        print(x.size())
+        x = torch.transpose(x, 2, 3)
         x = self.hidden1(x)
-        x = torch.transpose(x, 1, 2)
+        x = torch.transpose(x, 2, 3)
         x = self.rnn(x)
-        x = torch.transpose(x, 1, 2)
+        x = torch.transpose(x, 2, 3)
         x = self.hidden2(x)
-        x = torch.transpose(x, 1, 2)
+        x = torch.transpose(x, 2, 3)
         return x
