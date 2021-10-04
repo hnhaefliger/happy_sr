5fe6fb680dbe1fb27ce37b89e39849363e4b6a8c
hnhaefliger <henry_haefliger@protonmail.com>
2021-10-04 17:24:39 -0400

    change layer dimensions

--- happy_sr/model/model.py
+++ happy_sr/model/model.py
@@ -5,7 +5,7 @@ class Model(torch.nn.Module):
     def __init__(self, n_cnn_layers, n_rnn_layers, rnn_dim, n_class, n_feats, stride=2, dropout=0.1):
         super(Model, self).__init__()
         self.hidden1 = torch.nn.Sequential(
-            torch.nn.Linear(985, 1024),
+            torch.nn.Linear(1024, 1024),
             torch.nn.ReLU(),
             torch.nn.Dropout(0.05),
             torch.nn.Linear(1024, 1024),
