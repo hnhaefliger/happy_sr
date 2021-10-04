3f93e87819332874cea961232285ca2798f7b98c
hnhaefliger <henry_haefliger@protonmail.com>
2021-10-04 17:23:31 -0400

    change layer dimensions

--- happy_sr/model/model.py
+++ happy_sr/model/model.py
@@ -5,22 +5,22 @@ class Model(torch.nn.Module):
     def __init__(self, n_cnn_layers, n_rnn_layers, rnn_dim, n_class, n_feats, stride=2, dropout=0.1):
         super(Model, self).__init__()
         self.hidden1 = torch.nn.Sequential(
-            torch.nn.Linear(64, 256),
+            torch.nn.Linear(985, 1024),
             torch.nn.ReLU(),
-            torch.nn.Dropout(0.5),
-            torch.nn.Linear(256, 256),
+            torch.nn.Dropout(0.05),
+            torch.nn.Linear(1024, 1024),
             torch.nn.ReLU(),
-            torch.nn.Dropout(0.5),
-            torch.nn.Linear(256, 256),
+            torch.nn.Dropout(0.05),
+            torch.nn.Linear(1024, 1024),
             torch.nn.ReLU(),
-            torch.nn.Dropout(0.5),
+            torch.nn.Dropout(0.05),
         )
         self.rnn = torch.nn.Sequential(
-            torch.nn.RNN(256, 256, bidirectional=True),
-            torch.nn.Dropout(0.5),
+            torch.nn.RNN(512, 512, bidirectional=True),
+            torch.nn.Dropout(0.50),
         )
         self.hidden2 = torch.nn.Sequential(
-            torch.nn.Linear(256, 29)
+            torch.nn.Linear(512, 29)
         )
 
     def forward(self, x):
