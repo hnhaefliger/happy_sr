7397b5226fb17ff3b7ae7ad446bddf6bd70292db
hnhaefliger <henry_haefliger@protonmail.com>
2021-10-05 11:27:51 -0400

    fix wer and cer

--- happy_sr/model/utils.py
+++ happy_sr/model/utils.py
@@ -13,12 +13,13 @@ def text_to_int(text):
 
 
 def int_to_text(labels):
-    return ''.join([chars[label] for label in labels])
+    return ''.join([chars[int(label)] for label in labels])
 
 
 def get_optimizer(model, loader, epochs, learning_rate):
     optimizer = torch.optim.AdamW(model.parameters(), learning_rate)
-    scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=1e-6, steps_per_epoch=int(len(loader)), epochs=epochs, anneal_strategy='linear')
+    scheduler = torch.optim.lr_scheduler.OneCycleLR(
+        optimizer, max_lr=1e-6, steps_per_epoch=int(len(loader)), epochs=epochs, anneal_strategy='linear')
 
     return optimizer, scheduler
 
@@ -27,6 +28,7 @@ def get_loss():
     loss = torch.nn.CTCLoss(blank=28).to(device)
 
     return loss
+
 
 def levenshtein_distance(a, b):
     distances = np.zeros((len(a)+1, len(b)+1))
@@ -50,10 +52,11 @@ def levenshtein_distance(a, b):
                 ]) + 1
 
     return distances[len(a)][len(b)]
-    
+
 
 def greedy_decoder(output, labels, blank_label=28, collapse_repeated=True):
     decoded = []
+    targets = [[l.item() for l in label] for label in labels]
 
     for phrase in torch.argmax(output, dim=2):
         decoded.append([0])
@@ -61,11 +64,10 @@ def greedy_decoder(output, labels, blank_label=28, collapse_repeated=True):
         for arg in phrase:
             if arg != blank_label:
                 if not(collapse_repeated and decoded[-1][-1] == arg):
-                    decoded[-1].append(arg)
+                    decoded[-1].append(arg.item())
 
-    print(decoded, labels)
-
-    decoded, labels = [int_to_text(d) for d in decoded], [int_to_text(l) for l in labels]
+    decoded, labels = [int_to_text(d) for d in decoded], [
+        int_to_text(l) for l in targets]
 
     return decoded, labels
 
@@ -79,9 +81,14 @@ def error_rate(output, labels):
     return rates
 
 
+def avg(array):
+  return sum(array/len(array))
+
+
 def word_error_rate(output, labels):
-    return error_rate(greedy_decoder(output, labels).split(' '))
+    output, labels = greedy_decoder(output, labels)
+    return avg(error_rate([o.split(' ') for o in output], [l.split(' ') for l in labels]))
 
 
 def char_error_rate(output, labels):
-    return error_rate(greedy_decoder(output, labels))
+    return avg(error_rate(*greedy_decoder(output, labels)))
