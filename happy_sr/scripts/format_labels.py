6eb7ed309a5eff223fdc8d717a726ae3ff6bcdc0
hnhaefliger <henry_haefliger@protonmail.com>
2021-10-25 16:33:36 -0400

    prevent deletion of file headers

--- happy_sr/scripts/format_labels.py
+++ happy_sr/scripts/format_labels.py
@@ -3,7 +3,7 @@ print('reformatting training labels...')
 with open('./cv-valid-train.csv', 'r') as f_in:
     with open('./cv-valid-train/train.tsv', 'w+') as f_out:
         data = 'path,sentence' + f_in.read()[13:]
-        data = '\n'.join(['client_id,' + line for line in data.split('\n') if len(line.split(',')[0].split('/')[-1]) > 4])
+        data = '\n'.join(['client_id,' + line for line in data.split('\n') if (len(line.split(',')[0].split('/')[-1]) > 4 or 'path,sentence' in line)])
         data = data.replace(',', '\t')
         f_out.write(data)
 
@@ -13,7 +13,7 @@ print('reformatting evaluation labels...')
 with open('./cv-valid-test.csv', 'r') as f_in:
     with open('./cv-valid-test/test.tsv', 'w+') as f_out:
         data = 'path,sentence' + f_in.read()[13:]
-        data = '\n'.join(['client_id,' + line for line in data.split('\n') if len(line.split(',')[0].split('/')[-1]) > 4])
+        data = '\n'.join(['client_id,' + line for line in data.split('\n') if (len(line.split(',')[0].split('/')[-1]) > 4 or 'path,sentence' in line)])
         data = data.replace(',', '\t')
         f_out.write(data)
 
